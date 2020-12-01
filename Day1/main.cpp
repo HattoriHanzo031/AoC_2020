386127d9665941de94d00f5d0c9adf80c169dfbe
Filip Vranesevic <filip.vranesevic@seavus.com>
2020-12-01 19:58:23 +0100

    Day 1 Part 2 solution

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "FileInput.hpp"
+
 int main (int argc, char* argv[])
 {
 	(void) argc;
@@ -10,10 +11,14 @@ int main (int argc, char* argv[])
 
 	for (auto i = in.begin(); i != in.end(); i++) {
 		for (auto j = i+1; j != in.end(); j++) {
-			if (*i + *j == 2020) {
-				std::cout << "Resenje je: " << *i << " * " << *j << " = " << *i * *j << std::endl;
+			if (*i + *j >= 2020) continue; // Optimization
+			for (auto k = j+1; k != in.end(); k++) {
+				if (*i + *j + *k == 2020) {
+					std::cout << "Resenje je: " << *i << " * " << *j << " * " << *k << " = " << *i * *j * *k << std::endl;
+					return 0;
+				}
 			}
 		}
 	}
-	return 0;
+	return -1;
 }
