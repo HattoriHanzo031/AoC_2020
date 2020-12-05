86a324a0382355f089ebd19ad428dc558083934e
Filip Vranesevic <filip.vranesevic@seavus.com>
2020-12-05 08:21:22 +0100

    Day4 Part2 optimisation

--- Day4/main.cpp
+++ Day4/main.cpp
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <sstream>
 #include <algorithm>
-#include "FileInput.hpp"
 #include <regex>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
 
 class passport {
 	int byr;
 	int iyr;
 	int eyr;
-	std::string hgt;
+	int hgt;
 	std::string hcl;
 	std::string ecl;
 	std::string pid;
@@ -44,13 +47,26 @@ public:
                     eyr = -1;
             } else if (key == "hgt") {
                 sss >> hgt;
-                std::regex re("1[5-8][0-9]cm|19[0-3]cm|[5-6][0-9]in|[7][0-6]in");
+                std::string unit;
+                sss >> unit;
+                if (unit == "cm") {
+                    if ((hgt<150) || (hgt>193)) {
+                        hgt = -1;
+                    }
+                } else if (unit == "in") {
+                    if ((hgt<59) || (hgt>76)) {
+                        hgt = -1;
+                    }
+                } else {
+                    hgt = -1;
+                }
+                /*std::regex re("1[5-8][0-9]cm|19[0-3]cm|[5-6][0-9]in|[7][0-6]in");
                 if (!std::regex_match(hgt.c_str(), m, re)) {
                     hgt = "";
-                } 
+                }*/
             } else if (key == "hcl") {
                 sss >> hcl;
-                std::regex re("#[0-9a-fA-F]{6}");
+                std::regex re("#[0-9a-f]{6}");
                 if (!std::regex_match(hcl.c_str(), m, re)) {
                     hcl = "";
                 }
@@ -77,7 +93,7 @@ public:
         byr = -1;
         iyr = -1;
         eyr = -1;
-        hgt = "";
+        hgt = -1;
         hcl = "";
         ecl = "";
         pid = "";
@@ -87,7 +103,7 @@ public:
     bool isValid() const
     {
         //std::cout << byr << "\t\t" <<  iyr << "\t\t" <<  eyr << "\t\t" <<  hgt << "\t\t" <<  hcl << "\t\t" <<  ecl << "\t\t" <<  pid << "\t\t" <<  cid << std::endl;
-        return (byr != -1) && (iyr != -1) && (eyr != -1) && (hgt != "") && (hcl != "") && (ecl != "") && (pid != ""); 
+        return (byr != -1) && (iyr != -1) && (eyr != -1) && (hgt != -1) && (hcl != "") && (ecl != "") && (pid != "");
     }
 
 	operator bool() const
@@ -100,26 +116,19 @@ int main (int argc, char* argv[])
 {
 	(void) argc;
 	(void) argv;
-	FileInput input("Day4/input.txt");
+    std::ifstream file("Day4/input.txt");
 	std::string line;
     passport pas;
 	long num = 0;
 
-	std::vector<passport> list;
-
-	while (input.getNext(line)) {  // TODO: update
-
-        if (line.size() == 0) {
+	while (getline(file, line)) {
+        if (line.size() == 0 || file.peek() == EOF) {
             if (pas)
                 num++;
-            list.push_back(pas);
             pas.clear();
         }
         pas.update(line);
 	}
-
-    if (pas)
-        num++;
 
     std::cout << num << std::endl;
 	return 0;
