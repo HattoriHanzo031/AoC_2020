01477f1277b6317f9d483fdcc4ddfe8a00e0f68b
Filip Vranesevic <filip.vranesevic@seavus.com>
2020-12-06 13:29:11 +0100

    Day6 Part2 solution

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -1,32 +1,44 @@
 #include <string.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
-#define MAX_QUESTIONS ('z' - 'a' + 1)
+#define MAX_QUESTIONS ('z' - 'a' + 1) // number of possible questions
+
+int sumGroupAnswers(std::vector<std::string>& answers) {
+    unsigned numAnswers[MAX_QUESTIONS] = {0}; // number of answers for each possible question
+    int everyoneAnswered = 0;
+
+    for (auto answer : answers) {
+        for (char c : answer) {
+            int index = c -'a';
+            numAnswers[index]++;
+            // if (numAnswers[index] == 1) { // Part 1
+            if (numAnswers[index] == answers.size()) { // everyone answered this question
+                everyoneAnswered++;
+            }
+        }
+    }
+    return everyoneAnswered;
+}
 
 int main()
 {
     std::ifstream file("Day6/input.txt");
-    std::string line;
-    int sum = 0;
-    int numAnswers = 0;
-    bool answers[MAX_QUESTIONS] = {0};
+	std::string line;
+	long sum = 0;
+    std::vector<std::string> groupAnswers; // all answers of a group
 
 	while (getline(file, line)) {
-        for (char c : line) {
-            unsigned index = c - 'a';
-            if (index < MAX_QUESTIONS && answers[index] == false) {
-                answers[index] = true;
-                numAnswers++;
-            }
-        }
+        if (line.size() != 0)
+            groupAnswers.push_back(line);
 
         if (line.size() == 0 || file.peek() == EOF) {
-            sum += numAnswers;
-            numAnswers = 0;
-            memset(answers, 0, MAX_QUESTIONS);
+            sum += sumGroupAnswers(groupAnswers);
+            groupAnswers.clear();
         }
 	}
+
     std::cout << sum << std::endl;
     return 0;
 }
