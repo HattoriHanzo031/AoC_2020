fbde16c60a4e7c019b3c62ac1a9b285f8e6e5c24
Filip Vranesevic <filip.vranesevic@seavus.com>
2020-12-13 15:14:06 +0100

    Day12 Part2 solution

--- Day12/main.cpp
+++ Day12/main.cpp
@@ -2,13 +2,6 @@
 #include <fstream>
 #include <sstream>
 
-enum Direction {
-    N = 0,
-    S = 1,
-    E = 2, 
-    W = 3,
-};
-
 struct coordinate {
     int x;
     int y;
@@ -19,22 +12,14 @@ int main()
     std::string line;
     char c;
     int num;
+    int tmp;
     coordinate ship = {0, 0};
-
-    const coordinate dir[4] = {
-        { 0,  1}, // N
-        { 0, -1}, // S
-        { 1,  0}, // E
-        {-1,  0}  // W
-    };
+    coordinate waypoint = {10, 1};
 
     std::ifstream file("Day12/input.txt");
 
     if (!file.is_open())
         return -1;
-
-    enum Direction movingDir = E;
-    enum Direction shipDir = E;
 
     while (getline(file, line)) {
         std::stringstream ss(line);
@@ -43,37 +28,37 @@ int main()
         switch (c)
         {
         case 'N':
-            movingDir = N;
+            waypoint.y += num;
             break;
         case 'S':
-            movingDir = S;
+            waypoint.y -= num;
             break;
         case 'E' :
-            movingDir = E;
+            waypoint.x += num;
             break;
         case 'W' :
-            movingDir = W;
-            break;
-        case 'L' :
-            shipDir = (enum Direction)((shipDir + (num/90)) % 4);
-            movingDir = shipDir;
-            num = 0;
+            waypoint.x -= num;
             break;
         case 'R' :
-            shipDir = (enum Direction)((4 + shipDir - (num/90)) % 4);
-            movingDir = shipDir;
-            num = 0;
+            for (int i=0; i<num; i+=90) {
+                tmp = waypoint.y;
+                waypoint.y = - waypoint.x;
+                waypoint.x = tmp;
+            }
+            break;
+        case 'L' :
+            for (int i=0; i<num; i+=90) {
+                tmp = waypoint.y;
+                waypoint.y = waypoint.x;
+                waypoint.x = -tmp;
+            }
             break;
         case 'F' :
-            movingDir = shipDir;
+            ship.x += num*waypoint.x;
+            ship.y += num*waypoint.y;
         default:
             break;
         }
-
-
-        ship.x += num * (dir[movingDir].x);
-        ship.y += num * (dir[movingDir].y);
-        //std::cout << num << " " << dir[movingDir].x << " " << dir[movingDir].y << " - x:" << x << " y:" << y << std::endl;
     }
 
     std::cout << "x: " << ship.x << " y: " << ship.y << " Manhattan distance: " << std::abs(ship.x) + std::abs(ship.y) << std::endl;
